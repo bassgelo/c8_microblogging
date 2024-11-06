1a06a89482f0c4cce0aa765847d3b30510af302a
Gerardo Manzano <g.manzano@j-it.at>
2024-11-06 10:54:41 +0100

    Added comments for clarity

--- zeebe-worker_spring-zeebe/src/main/java/springzeebe/cloud/worker/ApproverWorker.java
+++ zeebe-worker_spring-zeebe/src/main/java/springzeebe/cloud/worker/ApproverWorker.java
@@ -18,11 +18,17 @@ public class ApproverWorker {
     @JobWorker(type = "processTwittter_approveMessage")
     public Map<String, Object> approveMessage(final ActivatedJob job) {
 
+        //3 Blocks of code
+        //Block 1: read variables from the process instance
         //Read variables from the process this way
         String message = (String)job.getVariablesAsMap().get("message");
 
+        //Block 2: call the actual logic
+        //It is better to call the logic not directly here in this class, instead use a bean that has the real logic, and pass the variables read from the process
+        //This enables the separation between, the actual business logic, and the code that is required by Camunda.
         boolean approved = randomBooleanService.getRandomBoolean(message);
 
+        //Block 3: return to the process instance the result of the processing (if necessary)
         // Return generated variable to Zeebe
         HashMap<String, Object> variables = new HashMap<>();
         variables.put("approved", approved);
